9cddcfbc56d90b656c5c2f6bf913f39a0f1ee13e
Divyansh Sharma <92170111+Dev4821@users.noreply.github.com>
2023-01-31 23:11:26 +0530

    Create Minimum times A has to be repeated such that B is a substring of it.java

---
+++ Minimum times A has to be repeated such that B is a substring of it.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int minRepeats(String A, String B) {
+         String c ="";
+        int count = 0;
+        while(c.length()<=2*B.length()){
+            c+=A;
+            count++;
+            if(c.contains(B)){
+                return count;
+            }
+        }
+        return -1;
+    }
+};
